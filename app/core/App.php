76475a721e26b0775f1d84bbea141e9e9e51667d
bernarduswillson <13521021@mahasiswa.itb.ac.id>
2023-09-27 00:35:06 +0700

    feat: add Home

---
+++ app/core/App.php
@@ -0,0 +1,46 @@
+<?php
+
+class App {
+    protected $controller = 'Home';
+    protected $method = 'index';
+    protected $params = [];
+  
+    public function __construct()
+    {
+      // Redirect to 'Home'
+      $url = $this->parse_url();
+      if ($url[0] != 'register') {
+        $this->controller = 'Home';
+      }
+      
+      // Load the controller
+      require_once 'app/controllers/' . $this->controller . '.php';
+      $this->controller = new $this->controller;
+  
+      if (isset($url[1])) {
+        if (method_exists($this->controller, $url[1])) {
+          $this->method = $url[1];
+          unset($url[1]);
+        }
+      }
+  
+      if ($url) {
+        $this->params = array_values($url);
+      }
+  
+      call_user_func_array([$this->controller, $this->method], $this->params);
+    }
+  
+    public function parse_url()
+    {
+      if (isset($_SERVER['REQUEST_URI'])) {
+        $url = rtrim($_SERVER['REQUEST_URI'], '/');
+        $url = ltrim($url, 'public/');
+        $url = filter_var($url, FILTER_SANITIZE_URL);
+        $url = ltrim($url, '?');
+        $url = explode('/', $url);
+        return $url;
+      }
+    }
+  }
+  
---
+++ app/views/home/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <body>
+        <div class="main-container">
+            <?php
+                echo "Hello, World!";
+            ?>
+        </div>
+    </body>
+</html>
