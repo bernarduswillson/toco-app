f0668291402c8f9a961d90bae2944f3444fe09ed
bewe <93899302+bernarduswillson@users.noreply.github.com>
2023-09-28 14:23:51 +0700

    feat: add 404 page

---
+++ app/views/Error404/index.php
@@ -0,0 +1,31 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../public/css/error.css">
+    <link rel="stylesheet" href="../../../public/css/global.css">
+</head>
+<body>
+    <div class="error">
+        <div>
+            <h1 class="error-header">
+                404
+            </h1>
+            <h2 class="error-desc">
+                A Temporary Language Barrier. <br> We'll Break It Together.
+            </h2>
+            <div class="button-container">
+                <a href="/">
+                    <button class="error-button">
+                        Go to homepage
+                    </button>
+                </a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
