8ff9580f9e7220fc5cf40d5d87bbac1dcb795d3d
Ditra Amadia <ditrarizqaamadia@gmail.com>
2023-09-30 13:50:51 +0700

    feat: standardize 404 code

--- app/controllers/Error404.php
+++ app/controllers/Error404.php
@@ -2,6 +2,9 @@
 
 class Error404 extends Controller {
   public function index() {
+    $data["pageTitle"] = "404 | Oops, error in translation";
+
+    $this->view('header/index', $data);
     $this->view('Error404/index');
   }
 }
