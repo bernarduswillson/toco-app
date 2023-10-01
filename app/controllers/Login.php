f90dc0dd6de30a31935f95d7460ac26fd6deca3b
bewe <93899302+bernarduswillson@users.noreply.github.com>
2023-10-01 20:28:17 +0700

    fix: login routing

--- app/controllers/Login.php
+++ app/controllers/Login.php
@@ -2,9 +2,17 @@
 
 class Login extends Controller {
   public function index() {
-    $data["pageTitle"] = "Login";
+    if ($this->isLoggedIn()) {
+      header('Location: /learn');
+      exit();
+    }
 
+    $data["pageTitle"] = "Login";
     $this->view('header/index', $data);
     $this->view('login/index');
+  }
+
+  private function isLoggedIn() {
+    return isset($_SESSION['user_id']);
   }
 }
