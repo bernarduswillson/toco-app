a945d71ca19d1d0ea5a11fd3aa6518a67f2891ba
Raditss <raditya.naufal.a@gmail.com>
2023-10-04 18:15:13 +0700

    fix css video

--- app/views/video/index.php
+++ app/views/video/index.php
@@ -1,10 +1,19 @@
-<div class="module-video">
-    <div class="video-container">
-        <h1 class="video-header text-xl font-bold text-blue-purple-gradient">
-            <?php echo "test"; ?>
-        </h1>
-        <div class="video-player">
-            <iframe src="<?php echo 'https://www.youtube.com/embed/ioldoJQYKyQ?si=0Eyexy_89R0AwY6K'; ?>" frameborder="0" allowfullscreen></iframe>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Video Page</title>
+    <link rel="stylesheet" href="../../../public/css/video.css">
+</head>
+<body>
+    <div class="module-video">
+        <div class="video-container">
+            <h1 class="video-header text-l font-bold text-blue-purple-gradient">
+                <?php echo "test"; ?>
+            </h1>
+            <div class="video-player">
+                <iframe src="<?php echo 'https://www.youtube.com/embed/ioldoJQYKyQ?si=0Eyexy_89R0AwY6K'; ?>" frameborder="0" allowfullscreen></iframe>
+            </div>
         </div>
     </div>
-</div>
+</body>
+</html>
