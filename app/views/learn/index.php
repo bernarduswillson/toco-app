397cfce90df12f74f200135bead7ffac8b12c512
Ditra Amadia <ditrarizqaamadia@gmail.com>
2023-10-07 21:26:10 +0700

    fix: fix learn page styling

--- app/views/learn/index.php
+++ app/views/learn/index.php
@@ -11,25 +11,23 @@
 </head>
 <body>
     <div class="learn">
-        <div class="learn1">
-            <h1 class="learn-header">
-                I want to learn ...
-            </h1>
-            <div class="learn-container">
-                <?php foreach ($data["languages"] as $language): ?>
-                    <div class="learn-card" onclick="submitForm(<?= $language["language_id"] ?>)">
-                        <form id="form_<?= $language["language_id"] ?>" action="../../../api/main/addProgress.php" method="post">
-                            <input type="hidden" name="language_id" value="<?= $language["language_id"] ?>">
-                            <div>
-                                <img class="learn-card-image" src="<?= $language["language_flag"] ?>" alt="<?= $language["language_name"] ?>" height="130px">
-                                <h2 class="learn-card-header">
-                                    <?= $language["language_name"] ?>
-                                </h2>
-                            </div>
-                        </form>
-                    </div>
-                <?php endforeach ?>
-            </div>
+        <h1 class="learn-header">
+            I want to learn ...
+        </h1>
+        <div class="learn-container">
+            <?php foreach ($data["languages"] as $language): ?>
+                <div class="learn-card" onclick="submitForm(<?= $language["language_id"] ?>)">
+                    <form id="form_<?= $language["language_id"] ?>" action="../../../api/main/addProgress.php" method="post">
+                        <input type="hidden" name="language_id" value="<?= $language["language_id"] ?>">
+                        <div>
+                            <img class="learn-card-image" src="<?= $language["language_flag"] ?>" alt="<?= $language["language_name"] ?>" height="130px">
+                            <h2 class="learn-card-header">
+                                <?= $language["language_name"] ?>
+                            </h2>
+                        </div>
+                    </form>
+                </div>
+            <?php endforeach ?>
         </div>
     </div>
 
